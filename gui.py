aefcaadfa2b5aa9927f216c0a9d749e6033922d4
SandroArmando23 <francesco.canepa@edu.immaginazioneelavoro.it>
2025-12-22 11:37:36 +0100

    Implemented the Complete and Exit button, day 18

--- gui.py
+++ gui.py
@@ -8,13 +8,17 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45,10])
 edit_button =sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 """
 Layout vuole una lista. All'interno della lista scriviamo altre liste e ognuna 
 corrisponde a una riga nella finestra. poi aggiungiamo font e si va a capo un po come in css
 """
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Segoe UI', 20))  # Tupla con due valori
 
 while True:
@@ -28,7 +32,7 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            window['todos'].update(values=todos)   #scrive nella input in tempo reale cio che clicchiamo
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -38,6 +42,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)      #aggiorna in tempo reale la lista nella finestra
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
